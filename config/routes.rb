c494561d3ccb42ab1c7cef86fd0c5379bb19d898
Jeffrey Koolbergen <jeffrey@boxture.com>
2021-12-23 11:39:11 +0100

    Start over

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   # GET /about
   get 'about-us', to: 'about#index', as: :about
 
-  get 'sign_up', to: 'registrations#new'
-  post 'sign_up', to: 'registrations#create'
-
   root to: 'main#index', as: :home
 end
